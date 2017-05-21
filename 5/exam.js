3583a63268c93de3ff693006bf5d8d36f2c86c39
Revin <oniduka_takeshi@yahoo.co.jp>
2017-05-21 11:59:52 +0900

    no message

---
+++ 5/exam.js
@@ -0,0 +1,4 @@
+a = 10;
+b = Math.abs(-55);
+c = a + b + Math.abs(-1);
+console.log(c);
---
+++ 6/answer_can_buy.js
@@ -0,0 +1,11 @@
+money = 100;
+
+price = 300;
+
+message = "買えません";
+if (money >= price) {
+  message = "買えます";
+}
+
+
+console.log(message);
