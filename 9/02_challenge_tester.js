168ed022b91bf7d0839b354fc0c7d6de6b4a6526
revin <oniduka_takeshi@yahoo.co.jp>
2017-06-08 17:55:20 +0900

    第8回,第9回の教材を整理

--- 9/02_challenge_tester.js
+++ 9/02_challenge_tester.js
@@ -4,20 +4,14 @@ func = function(){
   message = "サイコロの結果は" + dice;
   if (dice <= 5) {
     message = message + "で、クリティカルです！";
-    if (dice == 1) {
-      message = message + "すげえ！";
-    }
   }
   else if (96 <= dice) {
     message = message + "で、ファンブルです！"
-    if (dice == 100) {
-      message = message + "やっちまったな…";
-    }
   }
   else {
     message = message + "です！";
   }
-
+  
   return message;
 };
 
