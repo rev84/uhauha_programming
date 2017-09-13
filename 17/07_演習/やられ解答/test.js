6081d82e8932f2740cc086952ece480158871742
revin <oniduka_takeshi@yahoo.co.jp>
2017-09-13 14:56:13 +0900

    やられの解答を追加

---
+++ 17/07_演習/やられ解答/test.js
@@ -0,0 +1,28 @@
+main = function(){
+  $('#dice').css('top', '0px');
+  $('#dice').css('left', '0px');
+
+  // 0.1秒ごとにdiceGoAwayを実行
+  setInterval(diceGoAway, 100);
+};
+
+diceGoAway = function(){
+  var dicetop = getTop($('#dice'));
+  dicetop = dicetop + 3;
+  $('#dice').css('top', dicetop + 'px');
+  var diceleft = getLeft($('#dice'));
+  diceleft = diceleft + 3;
+  $('#dice').css('left', diceleft + 'px');
+};
+
+// topを数値で取得する
+getTop = function(jQueryElement) {
+  return Number($(jQueryElement).css('top').replace('px', ''));
+};
+
+// leftを数値で取得する
+getLeft = function(jQueryElement) {
+  return Number($(jQueryElement).css('left').replace('px', ''));
+};
+
+$().ready(main);
